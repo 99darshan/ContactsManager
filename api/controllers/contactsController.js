5e7ee0c54e2d2976dff518fdfe4654ba47ce7b60
99darshan <dsn.adh@gmail.com>
2019-11-09 22:23:37 +0545

    modified get and post requests to fetch and create contacts using postgres database

--- api/controllers/contactsController.js
+++ api/controllers/contactsController.js
@@ -37,37 +37,62 @@ const {pool} = require('../DAL/dbConfig');
 let contactsController = {
     getAllContacts: async (req, res,next)=>{
         try {
-            let result = await pool.query(`SELECT * from contacts;`);
-            console.table(result.table);
+            const result = await pool.query(`SELECT * FROM contacts;`);
             res.status(200).json(result.rows);
         } catch (error) {
-            res.status(500).json(error.message);
+            res.status(500).json(error);
         }
     },
 
-    getContactById: function(req, res,next){
-        const parsedId = parseInt(req.params.id); 
-        const contact = contacts.find(c => c.id === parsedId );
-        if(!contact) return res.status(404).json({message: `Contact with id ${parsedId} doesn't exists`});
-    res.status(200).json(contact);
-    },
-    
-    createContact: function(req,res,next){
-        const newContact = {
-            id: contacts.length+1,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            company: req.body.company,
-            email: req.body.email,
-            phoneNumber: req.body.phoneNumber,
-            address: req.body.address
+    getContactById: async (req, res,next) => {
+        try{
+            const parsedId = parseInt(req.params.id);
+            const result = await pool.query(`SELECT * FROM contacts where id=${parsedId}`);
+            if(result.rows.length === 0) return res.status(404).json({message: `Contact with id ${parsedId} doesn't exist.`});
+            res.status(200).json(result.rows);
+        }catch(error){
+            res.status(500).json(error);
         }
-        contacts.push(newContact);
-         
-        if(!newContact) return res.status(404).json({message: 'error creating new contact'});
-        res.status(201).json(newContact);
+    },    
+    createContact: async (req,res,next) => {
+        //TODO: validate and sanitize input request body to handle sql injection
+        // TODO: make sure app doesn't crash when client sends an unformatted JSON or an empty JSON in the request body, 
+        // TODO: make sure undefined values are not being inserted as undefined string into database
+        // TODO: send the link to newly created resource in response header??
+        
+        console.log(JSON.stringify(req.body));
+        try {
+            const newContact = {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                company: req.body.company,
+                email: req.body.email,
+                phoneNumber: req.body.phoneNumber,
+                address: req.body.address
+            };
+
+            let insertScript = `INSERT INTO contacts (firstname,lastname,phonenumber,company,email,address) VALUES ('${newContact.firstName}', '${newContact.lastName}', '${newContact.phoneNumber}', '${newContact.company}', '${newContact.email}', '${newContact.address}');`;
+
+            console.log(insertScript);
+            // TODO: validate and sanitize the req.body inputs, handle sql injection
+            const result = await pool.query(insertScript);
+            //if(result.rows.length === 0) return res.stat TODO:
+            res.status(201).json(result.rows);
+
+        } catch (error) {
+            res.status(500).json(error);
+        }
     },
-    deleteContact: function(req,res,next){},
+    deleteContact: async(req,res,next)=>{
+        try{
+            console.log('delete called');
+            const parsedId = parseInt(req.params.id);
+            const result = await pool.query(`Delete FROM contacts where id=${parsedId}`);
+            res.status(200);
+        }catch(error){
+            res.status(500).json(error);
+        }
+    },
     updateContact: function(req,res,next){}
 
 };
