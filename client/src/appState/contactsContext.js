048b2cd889d2cd2c004e82d98a627cda8afbaf78
99darshan <dsn.adh@gmail.com>
2019-11-13 19:49:09 +0545

    enhanced useEffect method and reducers for state management

--- client/src/appState/contactsContext.js
+++ client/src/appState/contactsContext.js
@@ -7,27 +7,53 @@ export const ContactsContext = createContext();
 // useReducer hooks returns state and dispatch which are passed down as values from Providers
 // reducer and initialState are passed as props to the ContactsProvider
 export default function ContactsProvider(props){
-    let initialState = {contacts:[], isFetching: true}
+    let initialState = {contacts:[], isFetching: true, hasError: false}
     let [state, dispatch] = useReducer(contactsReducer, initialState); 
 
     // Fetch initialContacts array from the server TODO:
     // second empty [] makes this effect behave as componentDidMount else it will also be called on componentDidUpdate and fetching data from api in every request.
-    useEffect(async() => {
-        try {
-            // TODO: handle error, check status code??
-            let response = await fetch('http://localhost:3000/contacts');
-            let res = await response.json();
-            dispatch({
-                type:"FETCH_ALL_CONTACTS",
-                payload: res
-            });
-            //initialState = {...initialState, contacts:[...res], isFetching:false}
-            console.table(res);
-            console.table(initialState);
-            
-        } catch (error) {
-            console.log(error.message);
-        }
+    // TODO: shoudl return a function, fix this,add loading and error to the state
+    useEffect(() => {
+        
+        async function fetchData(){
+            try {
+                // TODO: port to base url, what if status code other than 200 or 500
+                let response = await fetch('http://localhost:3000/contacts');
+                if(response.status ===  200)
+                {
+                    let res = await response.json();
+                    dispatch({
+                      type: "FETCH_ALL_CONTACTS",
+                      payload: {res, isFetching: false}
+                    });
+                    
+                }
+
+                if(response.status === 500)
+                {
+                    let res = await response.json();
+                    dispatch({
+                        type:"ERROR",
+                        payload:{res}
+                    })
+                }
+                
+                //initialState = {...initialState, contacts:[...res], isFetching:false}
+                //console.table(res);
+                console.table(initialState);
+                
+            } catch (error) {
+                console.log(error.message);
+                dispatch({
+                    type:"ERROR",
+                    payload:{message: "Error Fetching Data From The Api"}
+                })
+            }
+        };
+
+        fetchData();
+
+        
       }, []);
 
     return (<ContactsContext.Provider value={{state, dispatch}}>
--- client/src/appState/contactsReducer.js
+++ client/src/appState/contactsReducer.js
@@ -2,10 +2,8 @@ export const contactsReducer = (state, action) =>{
     switch(action.type){
         case 'ADD':
             return {...state, contacts:[...state.contacts, action.payload]}
-        case 'FETCH_ALL_CONTACTS':
-            console.table(action.payload);
-            console.log({...state, contacts:[ ...action.payload]});
-            return {...state, contacts:[ ...action.payload]}
+        case 'FETCH_ALL_CONTACTS':    
+            return {...state, contacts:[ ...action.payload.res], isFetching: action.payload.isFetching}
         default:
             return state;
     }
