01e20860af1da78b0091e2f0c032685614f6aa9d
99darshan <dsn.adh@gmail.com>
2019-11-15 08:02:21 +0545

    updated api controller, added put and delete routes, modified query to handle SQL injection, sanitized the request body data, added sucess and error response json format

--- api/controllers/contactsController.js
+++ api/controllers/contactsController.js
@@ -1,78 +1,195 @@
 //TODO: REMOVE contacts list
-const {pool} = require('../DAL/dbConfig');
-const contactsResponseMaker = require('../helpers/responseMaker');
-
-
+const { pool } = require("../DAL/dbConfig");
+const contactsResponseMaker = require("../helpers/responseMaker");
 
 let contactsController = {
-    getAllContacts: async (req, res,next)=>{
-        try {
-            const result = await pool.query(`SELECT * FROM contacts;`);
-            res.status(200).json(contactsResponseMaker.OK(req, result.rows));
-        } catch (error) {
-            console.log(error.message);
-            res.status(500).json(error);
-        }
-    },
-    getContactById: async (req, res,next) => {
-        try{
-            const parsedId = parseInt(req.params.id);
-            const result = await pool.query(`SELECT * FROM contacts where id=${parsedId}`);
-            if(result.rows.length === 0) return res.status(404).json({message: `Contact with id ${parsedId} doesn't exist.`});
-            console.log(result.rows);
-            res.status(200).json(result.rows);
-        }catch(error){
-            res.status(500).json(error);
-        }
-    },    
-    createContact: async (req,res,next) => {
-        //TODO: validate and sanitize input request body to handle sql injection
-        // TODO: make sure app doesn't crash when client sends an unformatted JSON or an empty JSON in the request body, 
-        // TODO: make sure undefined values are not being inserted as undefined string into database
-        // TODO: send the link to newly created resource in response header?? and send error when required data are not received
-        
-        console.log(JSON.stringify(req.body));
-        try {
-            const newContact = {
-                firstName: req.body.firstName || '',
-                lastName: req.body.lastName || '',
-                company: req.body.company || '',
-                email: req.body.email || '',
-                phoneNumber: req.body.phoneNumber,
-                address: req.body.address || '',
-                birthday: req.body.birthday || null
-            };
-            console.log(newContact);
-            const parsedBirthday = newContact.birthday === null ? null :  `'${newContact.birthday}'`
-            console.log(parsedBirthday);
+  getAllContacts: async (req, res, next) => {
+    try {
+      const result = await pool.query(`SELECT * FROM contacts;`);
+      res.status(200).json(contactsResponseMaker.success(req, result.rows));
+    } catch (error) {
+      res.status(500).json(contactsResponseMaker.error(req, error));
+    }
+  },
+  getContactById: async (req, res, next) => {
+    try {
+      const parsedId = parseInt(req.params.id);
+      //NOTE: parseInt parses "1dbd" as 1 so use regex check instead to make sure the id path param is numeric
+        // if(isNaN(parsedId)) return res.status(400).json(contactsResponseMaker.error(req, {message:  `no contacts exists for ${req.params.id}`}));
+      if (!new RegExp("^[0-9]+$").test(req.params.id))
+        return res.status(400).json(
+          contactsResponseMaker.error(req, {
+            message: `Invalid path params`
+          })
+        );
 
-            let insertScript = `INSERT INTO contacts (first_name,last_name,phone_number,company,email,address,birthday) VALUES ('${newContact.firstName}', '${newContact.lastName}', '${newContact.phoneNumber}', '${newContact.company}', '${newContact.email}', '${newContact.address}', ${parsedBirthday}) RETURNING *;`;
+      const selectQuery = {
+        text: `SELECT * FROM contacts where id=$1`,
+        values: [parsedId]
+      };
+      const result = await pool.query(selectQuery);
+      if (result.rows.length === 0)
+        return res.status(404).json(
+          contactsResponseMaker.error(req, {
+            message: `Contact with id ${parsedId} doesn't exist.`
+          })
+        );
+      res.status(200).json(contactsResponseMaker.success(req, result.rows));
+    } catch (error) {
+      res.status(500).json(contactsResponseMaker.error(req, error));
+    }
+  },
+  createContact: async (req, res, next) => {
+    console.log(JSON.stringify(req.body));
+    try {
+      // NOTE: BUG TODO: !req.body.phoneNumber would consider 0 as empty as well
+      if (!req.body.firstName || !req.body.phoneNumber)
+        return res.status(400).json(
+          contactsResponseMaker.error(req, {
+            message:
+              "Invalid Request!! firstName and phoneNumber should be present in the request body."
+          })
+        );
 
-            console.log(insertScript);
-            
-            const result = await pool.query(insertScript);
-            //console.table(result);
-            console.log(result.rows);
-            //if(result.rows.length === 0) return res.stat TODO:
-            res.status(201).json(contactsResponseMaker.OK(req, result.rows));
+      if (!new RegExp("^[0-9]+$").test(req.body.phoneNumber))
+        return res.status(400).json(
+          contactsResponseMaker.error(req, {
+            message:
+              "Invalid Request!! Phone number should only contain numbers."
+          })
+        );
 
-        } catch (error) {
-            res.status(500).json(error);
-        }
-    },
-    deleteContact: async(req,res,next)=>{
-        try{
-            // TODO: return 200 if the resource to be deleted doesn't exist??
-            console.log('delete called');
-            const parsedId = parseInt(req.params.id);
-            const result = await pool.query(`Delete FROM contacts where id=${parsedId}`);
-            res.sendStatus(200);
-        }catch(error){
-            res.status(500).json(error);
-        }
-    },
-    updateContact: function(req,res,next){}
+      const newContact = {
+        firstName: req.body.firstName || "",
+        lastName: req.body.lastName || "",
+        company: req.body.company || "",
+        email: req.body.email || "",
+        phoneNumber: req.body.phoneNumber,
+        address: req.body.address || "",
+        birthday: req.body.birthday || null
+      };
+      //console.log(newContact);
+      //const parsedBirthday = newContact.birthday === null ? null :  `'${newContact.birthday}'`
+      //console.log(parsedBirthday);
 
+      //let insertScript = `INSERT INTO contacts (first_name,last_name,phone_number,company,email,address,birthday) VALUES ('${newContact.firstName}', '${newContact.lastName}', '${newContact.phoneNumber}', '${newContact.company}', '${newContact.email}', '${newContact.address}', ${parsedBirthday}) RETURNING *;`;
+
+      const insertQuery = {
+        text:
+          "INSERT INTO contacts (first_name,last_name,phone_number,company,email,address,birthday) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+        values: [
+          newContact.firstName.trim(),
+          newContact.lastName.trim(),
+          newContact.phoneNumber,
+          newContact.company.trim(),
+          newContact.email.trim(),
+          newContact.address.trim(),
+          newContact.birthday
+        ]
+      };
+
+      const result = await pool.query(insertQuery);
+      console.log(result.rows);
+      res.status(201).json(contactsResponseMaker.success(req, result.rows));
+    } catch (error) {
+      res.status(500).json(contactsResponseMaker.error(req, error));
+    }
+  },
+  deleteContact: async (req, res, next) => {
+    try {
+      if (!new RegExp("^[0-9]+$").test(req.params.id)) {
+        return res.status(400).json(
+          contactsResponseMaker.error(req, {
+            message: `Invalid path params`
+          })
+        );
+      }
+
+      const parsedId = parseInt(req.params.id);
+
+      const selectQuery = {
+        text: `SELECT * FROM contacts where id=$1`,
+        values: [parsedId]
+      };
+      const selectResult = await pool.query(selectQuery);
+      if (selectResult.rows.length === 0) {
+        return res.status(404).json(
+          contactsResponseMaker.error(req, {
+            message: `Deletion Failed!! Contact with id ${parsedId} doesn't exist.`
+          })
+        );
+      }
+
+      const deleteQuery = {
+        text: `DELETE FROM contacts where id = $1`,
+        values: [parsedId]
+      };
+      const deleteResult = await pool.query(deleteQuery);
+      res
+        .status(200)
+        .json(contactsResponseMaker.success(req, deleteResult.rows));
+    } catch (error) {
+      res.status(500).json(contactsResponseMaker.error(req, error));
+    }
+  },
+  updateContact: async function(req, res, next) {
+    try {
+      if (!new RegExp("^[0-9]+$").test(req.params.id)) {
+        return res.status(400).json(
+          contactsResponseMaker.error(req, {
+            message: `Invalid path params`
+          })
+        );
+      }
+
+      const parsedId = parseInt(req.params.id);
+
+      const selectQuery = {
+        text: `SELECT * FROM contacts where id=$1`,
+        values: [parsedId]
+      };
+      const selectResult = await pool.query(selectQuery);
+      if (selectResult.rows.length === 0) {
+        return res.status(404).json(
+          contactsResponseMaker.error(req, {
+            message: `Update Failed!! Contact with id ${parsedId} doesn't exist.`
+          })
+        );
+      }
+
+      const updatedContact = {
+        firstName: req.body.firstName || "",
+        lastName: req.body.lastName || "",
+        company: req.body.company || "",
+        email: req.body.email || "",
+        phoneNumber: req.body.phoneNumber,
+        address: req.body.address || "",
+        birthday: req.body.birthday || null
+      };
+
+      const updateQuery = {
+        text:
+          "UPDATE contacts set first_name = $1, last_name = $2, phone_number = $3, company = $4, email = $5, address = $6, birthday = $7 where id = $8 RETURNING *",
+        values: [
+          updatedContact.firstName.trim(),
+          updatedContact.lastName.trim(),
+          updatedContact.phoneNumber,
+          updatedContact.company.trim(),
+          updatedContact.email.trim(),
+          updatedContact.address.trim(),
+          updatedContact.birthday,
+          parsedId
+        ]
+      };
+
+      const updateResult = await pool.query(updateQuery);
+      res
+        .status(200)
+        .json(contactsResponseMaker.success(req, updateResult.rows));
+    } catch (error) {
+      res.status(500).json(contactsResponseMaker.error(req, error));
+    }
+  }
 };
 
 module.exports = contactsController;
--- api/helpers/responseMaker.js
+++ api/helpers/responseMaker.js
@@ -1,24 +1,35 @@
 /**
- * creates response for contacts resource using HATEOS ION spec
+ * creates success and error response for contacts resource using HATEOS ION spec
  * converts snake_cases to camelCases
  */
 const contactsResponseMaker = {
-    OK: (req, results) => {
-        let responseJson = {
-            href: req.path,
-            values: results.map(item => ({
-              id: item.id,
-              firstName: item.first_name || "",
-              lastName: item.last_name || "",
-              phoneNumber: item.phone_number || "",
-              company: item.company || "",
-              email: item.email || "",
-              address: item.address || "",
-              birthday: item.birthday || ""
-            }))
-          };
-          return Object.freeze(responseJson);
-    }
-}
+  success: (req, results) => {
+    let responseJson = {
+      self: req.path,
+      values: results.map(item => ({
+        self: `${req.path}/${item.id}`,
+        id: item.id,
+        firstName: item.first_name || "",
+        lastName: item.last_name || "",
+        phoneNumber: item.phone_number || "",
+        company: item.company || "",
+        email: item.email || "",
+        address: item.address || "",
+        birthday: item.birthday || ""
+      }))
+    };
+    return Object.freeze(responseJson);
+  },
+  error: (req, error) => {
+      let responseJson = {
+          self: req.path,
+          error: {
+              ...error,
+              message: error.message
+          }
+      }
+      return Object.freeze(responseJson);
+  }
+};
 
 module.exports = contactsResponseMaker;
