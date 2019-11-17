5d77811948676622e12c828b6dbb0d0c6088ef63
99darshan <dsn.adh@gmail.com>
2019-11-18 02:56:53 +0545

    db config changes for heroku connectionstring

--- api/configs/dbConfig.js
+++ api/configs/dbConfig.js
@@ -2,17 +2,24 @@ require('dotenv').config();
 const {Pool} = require('pg');
 
 const isProd = process.env.NODE_ENV === 'production';
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-//console.log(`connection string: ${connectionString}`);
-const dbConfig = {
-  //connectionString: connectionString,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE
-};
+let dbConfig;
+if(isProd){
+   dbConfig = 
+    {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
+      };
+}else{
+   dbConfig = {
+    //connectionString: connectionString,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE
+  };
+}
 
 const pool = new Pool(dbConfig);
 module.exports = { pool }
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-// });
-
+router.get('/', function(req, res, next) {
+    res.status(200).send("HOME ROUTE... CONTACTS API IS SERVED AT /contacts");
+});
 module.exports = router;
