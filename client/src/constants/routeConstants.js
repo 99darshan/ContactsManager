baf4be75f063f3c5c0ef5105aa8fd6b69813b9e3
99darshan <dsn.adh@gmail.com>
2019-11-18 01:44:14 +0545

    added heroku api path | removed local ssl cert

--- client/src/constants/routeConstants.js
+++ client/src/constants/routeConstants.js
@@ -4,4 +4,5 @@ export const ADD ="/contacts/add";
 export const EDIT = "/contacts/edit/:id";
 export const DETAILS = "/contacts/details/:id";
 
-export const API_BASE_URL = "https://localhost:5000";
+//export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "https://contacts-manager-api.herokuapp.com";
